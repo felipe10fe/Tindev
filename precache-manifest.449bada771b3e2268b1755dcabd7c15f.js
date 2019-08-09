375958f687e55c00678975c05f7b48f5f882f231
OsFlash <felipe_fzd10@hotmail.com>
2019-08-09 02:55:54 -0300

    build interface web

---
+++ precache-manifest.449bada771b3e2268b1755dcabd7c15f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcbb3c4702475ad573ab5328b95cae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98afbe729743bdf27daa",
+    "url": "/static/css/main.a82b5c60.chunk.css"
+  },
+  {
+    "revision": "294de1519d8b6a6ae2f3",
+    "url": "/static/js/2.dd2f9804.chunk.js"
+  },
+  {
+    "revision": "98afbe729743bdf27daa",
+    "url": "/static/js/main.8e05b387.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63d6e0613cb57065d4609e7f6f8d160c",
+    "url": "/static/media/happy.63d6e061.svg"
+  },
+  {
+    "revision": "c374cb18634e2b670e857151e93733a1",
+    "url": "/static/media/logo.c374cb18.svg"
+  },
+  {
+    "revision": "44bda2f39bffc90b5dce789311e24666",
+    "url": "/static/media/sad.44bda2f3.svg"
+  }
+]);
